3f3a84ff2c8e3b00191e2f096d055a045e3d81c7
ruc064 <33141912+ruc064@users.noreply.github.com>
2017-10-26 20:55:51 -0700

    Update web.js

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var interestText = document.getElementById("interests-text");
 var subjectArray = ['Tennis', 'Swimming', 'Piano', 'Competitive Gaming']; 
 var idArray = ['interests-tennis', 'interests-swim', 'interests-piano', 'interests-league'];
 var right = document.getElementById("right"); 
-var subjectColor = ['#88cfe2', '#515f63', '#5edbe5', 'black'];
+var subjectColor = ['#88cfe2', '#515f63', '#5edbe5', '#27b5ba'];
 var counter = 1;
 var arrCounter = 0;
                 
